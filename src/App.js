afc921ca3e0c9eda0a0d489514b3b99adc15a98e
RandallHammond23 <hrandall31@gmail.com>
2021-11-04 21:45:18 -0500

    added search bar, probably will change it to the aerch bar file later.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import navBar from './components/NavBar/NavBar';
+
+
+
 
 class App extends Component{
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      music: [],
+    };
   }
 
 
 
+  async componentDidMount() {
+    await axios.get(`http://www.devcodecampmusiclibrary.com/`) 
+  
+  }
+
+
 
   render(){
+    return (
+      <div> <navBar/> </div>
+    )
     
+
   }
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
-      </div>
-    </div>
-  </div>
-</nav>
+
+function navBar(){
+    return(
+
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Navbar</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="Home">Home</a>
+                            <form class="d-flex">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                </nav>
+
+    )
+
+}
+
+export default navBar
